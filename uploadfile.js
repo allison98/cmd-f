da7d963e612b321c42d4bae2c675ef483b3e7bc3
allison98 <allison98@gmail.com>
2019-03-09 19:57:56 -0800

    uploadfile with transcribing

--- uploadfile.js
+++ uploadfile.js
@@ -2,24 +2,64 @@ var http = require('http');
 var formidable = require('formidable');
 var fs = require('fs');
 
-http.createServer(function (req, res) {
-  if (req.url == '/fileupload') {
-    var form = new formidable.IncomingForm();
-    form.parse(req, function (err, fields, files) {
-      var oldpath = files.filetoupload.path;
-      var newpath = 'C:/Users/allisony/' + files.filetoupload.name;
-      fs.rename(oldpath, newpath, function (err) {
-        if (err) throw err;
-        res.write('File uploaded and moved!');
-        res.end();
-      });
- });
-  } else {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-    res.write('<input type="file" name="filetoupload"><br>');
-    res.write('<input type="submit">');
-    res.write('</form>');
-    return res.end();
-  }
-}).listen(8080);
+async function main() {
+  // Imports the Google Cloud client library
+  const speech = require('@google-cloud/speech');
+  const fs = require('fs');
+
+
+//create a server
+  var filename = http.createServer(function (req, res) {
+    if (req.url == '/fileupload') {
+      var form = new formidable.IncomingForm();
+      form.parse(req, function (err, fields, files) {
+        var oldpath = files.filetoupload.path;
+        var newpath = '/cmdfaudio' + files.filetoupload.name;
+        
+        fs.rename(oldpath, newpath, function (err) {
+          if (err) throw err;
+          res.write('File uploaded and moved!');
+          res.end();
+          return files.filetoupload.name;
+        });
+   });
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+      res.write('<input type="file" name="filetoupload"><br>');
+      res.write('<input type="submit">');
+      res.write('</form>');
+      return res.end();
+    }
+  }).listen(8080);
+
+
+  // Creates a client
+  const client = new speech.SpeechClient();
+
+  // The audio file's encoding, sample rate in hertz, and BCP-47 language code
+  const audio = {
+    uri: `gs://cmdfaudio/${filename}`,
+  };
+  const config = {
+    encoding: 'FLAC',
+    sampleRateHertz: 22050,
+    languageCode: 'en-US',
+  };
+  const request = {
+    audio: audio,
+    config: config,
+  };
+  // Detects speech in the audio file. This creates a recognition job that you
+  // can wait for now, or get its result later.
+  const [operation] = await client.longRunningRecognize(request);
+  // Get a Promise representation of the final result of the job
+  const [response] = await operation.promise();
+  const transcription = response.results
+    .map(result => result.alternatives[0].transcript)
+    .join('\n');
+  console.log(`Transcription: ${transcription}`);
+}
+main().catch(console.error);
+
+
